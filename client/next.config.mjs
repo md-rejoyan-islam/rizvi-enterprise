23cf17b2a144dab18f8d55d72f7462d55988e14e
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-09-25 01:07:10 +0600

    added readme file2

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     // SERVER_URL: "http://localhost:8080",
-    SERVER_URL: "http://www.rizvi.watchphase.com",
+    SERVER_URL: "http://rizvi.watchphase.com",
   },
 };
 
